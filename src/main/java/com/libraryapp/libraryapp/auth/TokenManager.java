80d629eabd0fcff96263119dd2105893c10f87a4
kya20 <82099378+kya20@users.noreply.github.com>
2021-08-16 10:54:11 +0300

    move TokenManager

--- src/main/java/com/libraryapp/libraryapp/auth/TokenManager.java
+++ src/main/java/com/libraryapp/libraryapp/auth/TokenManager.java
@@ -1,35 +1,55 @@
 package com.libraryapp.libraryapp.auth;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import lombok.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class TokenManager {
 
     /**
      * HS256 olacak şekilde bir key oluşturulmalı
-     private Key key  =??
-
-     *Token expired için 5 dakika
-     private final int expirationTime = 5*60*1000;
+     * private Key key  =??
+     * <p>
+     * Token expired için 5 dakika
+     * private final int expirationTime = 5*60*1000;
      */
+    private static final long serialVersionUID = -2550185165626007488L;
 
-    public String generateToken(String userMail){
-    //key ve expirationTime parametreleri token oluşturulurken kullanılacak.
-        return null;
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 1000;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String getUsermailFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
     }
 
-    public boolean validateToken(String token){
-      //isExpired
-     return  false;
+    public String generateToken(UserDetails userDetails) {
+
+        Map<String, Object> claims = new HashMap<>();
+        return GenerateToken(claims, userDetails.getUsermail());
     }
 
-    public String getUserMailToken(String token){
-
-        return null;
+    private Boolean isTokenExpired(String token) {
+        final Claims claims = getClaims(token);
+        return claims.getExpiration().before(new Date(System.currentTimeMillis()));
     }
 
-    //Claim işlemi/methodu olcak.
+    public boolean validateToken(String token) {
+        return Objects.nonNull(getClaims(token).getSubject()) && !isTokenExpired(token);
+
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
 
 }
